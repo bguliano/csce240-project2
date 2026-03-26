4c9b9f99e13687c0e0ab2dcd96e9c1eea823cce3
Braden Guliano <72895782+bguliano@users.noreply.github.com>
2026-03-26 13:40:55 -0400

    main boilerplate

--- main.cc
+++ main.cc
@@ -1,4 +1,10 @@
+// Copyright 2026 bguliano and erin-owens
+
 #include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
